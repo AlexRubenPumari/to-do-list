bb67b11717e59b9387d335f646fd373c1ace3289
Alex Pumari <alexpumari0@gmail.com>
2025-02-06 09:46:44 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+
+function App() {
+  return (
+    <div className='modal__overlay'>
+      <div className='modal'>
+        <h1>Hola mundo</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
